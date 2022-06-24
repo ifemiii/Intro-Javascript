200c589e1b3973194803ba2fd487b6d81be3f039
Ifemiii <ifeoluwaogungbemi.e@gmail.com>
2022-06-24 15:27:55 +0100

    Displays details using console and alert

---
+++ index.js
@@ -0,0 +1,14 @@
+let myName, height, country;
+
+myName = 'Ifeoluwa Ogungbemi';
+height = '175cm';
+country = 'Nigeria';
+
+console.log("Name: ",myName);
+console.log("Height: ",height);
+console.log("Country: ",country);
+
+
+alert("Name: " + myName);
+alert("Height: " + height);
+alert("Country: " + country);
